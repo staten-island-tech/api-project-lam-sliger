2d7c41c05cbe81dfa929951726f07f1f472f3e65
GavinL04 <71574591+GavinL04@users.noreply.github.com>
2021-05-16 03:18:04 -0400

    stuck

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "e0f5ea7719664f64b7a9b7dfcf251380";
+
+const query = async function () {
+  try {
+    const result = await fetch(
+      " https://www.bungie.net/Platform/Destiny2/0/Profile/28007537/Item/{itemInstanceId}/.gte=8&api_key=${key}"
+    );
+    const data = await Response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    alert("something went wrong");
+  }
+};
+query();
