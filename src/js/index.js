abc8c3bba3484d5687b39bfea4b3286e0676265d
ryan-sliger <ryan.sliger22@sitechhs.com>
2021-05-17 19:12:00 -0400

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -5,14 +5,24 @@ const key = "2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649";
 const query = async function () {
   try {
     const response = await fetch(
-      "https://www.worldcoinindex.com/apiservice/json?key=2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649"
+      "https://www.worldcoinindex.com/apiservice/ticker?2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649&label=ethbtc-ltcbtc&fiat=btc"
     );
     const data = await response.json();
-    data.results.forEach((coin) => {
-      DOMSelectors.grid.insertAdjacentHTML("beforeend", "html");
+    data.Markets.forEach((coin) => {
+      console.log(Markets);
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `
+      </div>
+      <h3 class="currenecy-name">${Name}</h3>
+        <p class="price-to-USD">${Price}}</p>
+        <p class="Price-to-Bitcoin">${Volume_24h}</p>
+    </div>`
+      );
+      console.log(Markets);
     });
   } catch (error) {
-    console.log(error);
+    //console.log(error);
     alert("something went wrong");
   }
 };
