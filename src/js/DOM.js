bf57a053784ee9339b573a804d1c599c24cd3116
ryan-sliger <ryan.sliger22@sitechhs.com>
2021-05-25 09:54:15 -0400

    class changes 5/25

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,8 @@
 const DOMSelectors = {
   grid: document.querySelector(".currency-name"),
+  grid: document.querySelector(".movie-grid"),
+  searchForm: document.getElementById("search-form"),
+  searchArea: document.getElementById("search-area"),
 };
 
 export { DOMSelectors };
