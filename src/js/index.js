523b7a77494f526747cc878dc95af407e64f7f64
ryan-sliger <ryan.sliger22@sitechhs.com>
2021-05-25 20:01:03 -0400

    at home changes

--- src/js/index.js
+++ src/js/index.js
@@ -5,31 +5,14 @@ const key = "2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649";
 const query = async function () {
   try {
     const response = await fetch(
-<<<<<<< HEAD
-      "https://www.worldcoinindex.com/apiservice/ticker?2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649&label=ethbtc-ltcbtc&fiat=btc"
-    );
-    const data = await response.json();
-    data.Markets.forEach((coin) => {
-      console.log(Markets);
-      DOMSelectors.grid.insertAdjacentHTML(
-        "beforeend",
-        `
-      </div>
-      <h3 class="currenecy-name">${Name}</h3>
-        <p class="price-to-USD">${Price}}</p>
-        <p class="Price-to-Bitcoin">${Volume_24h}</p>
-    </div>`
-      );
-      console.log(Markets);
-    });
-=======
       "https://www.worldcoinindex.com/apiservice/v2getmarkets?key=2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649&fiat=btc "
     );
     const data = await response.json();
-    console.log(data.Markets)
+    console.log(data.Markets);
     data.Markets.forEach((Coin) => {
-      DOMSelectors.grid.insertAdjacentHTML("beforeend", 
-      `<section class="currency-name">
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<section class="currency-name">
       <div class="price-to-USD">
        <div class="price-to-Bitcoin">
      
@@ -39,12 +22,11 @@ const query = async function () {
            <p class="Price-to-Bitcoin">${Coin.Volume_24h}</p>
        </div>
      </div> 
-   </section>`)
-   
-    });  
->>>>>>> 7e60c54094bffae6cd3c1391ae1acac124ae6473
+   </section>`
+      );
+    });
   } catch (error) {
-    //console.log(error);
+    console.log(error);
     alert("something went wrong");
   }
 };
--- src/js/search.js
+++ src/js/search.js
@@ -10,23 +10,14 @@ const listen = function () {
     const searchQuery = async function () {
       try {
         const response = await fetch(
-          `https://www.worldcoinindex.com/apiservice/v2getmarkets?key=2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649&fiat=btc`
+          "https://www.worldcoinindex.com/apiservice/v2getmarkets?key=2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649&fiat=btc "
         );
         const data = await response.json();
+        console.log(data.Markets);
         data.Markets.forEach((Coin) => {
-          let genreArr = [];
-          const addGenre = function () {
-            genres.forEach((element) => {
-              if (movie.genre_ids.includes(element.id)) {
-                genreArr.push(element.name);
-                return genreArr;
-              }
-            });
-          };
-          addGenre();
-
-          DOMSelectors.grid.insertAdjacentHTML("beforeend", 
-          `<section class="currency-name">
+          DOMSelectors.grid.insertAdjacentHTML(
+            "beforeend",
+            `<section class="currency-name">
           <div class="price-to-USD">
            <div class="price-to-Bitcoin">
          
@@ -36,11 +27,12 @@ const listen = function () {
                <p class="Price-to-Bitcoin">${Coin.Volume_24h}</p>
            </div>
          </div> 
-       </section>`);
+       </section>`
+          );
         });
       } catch (error) {
         console.log(error);
-        alert("Hey something went wrong");
+        alert("something went wrong");
       }
     };
     searchQuery();
