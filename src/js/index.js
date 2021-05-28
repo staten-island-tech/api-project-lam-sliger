81b03e8f021aa10f826d7a0c7e202b02faf9cd61
ryan-sliger <ryan.sliger22@sitechhs.com>
2021-05-27 22:18:07 -0400

    more changes

--- src/js/index.js
+++ src/js/index.js
@@ -5,27 +5,26 @@ const key = "2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649";
 const query = async function () {
   try {
     const response = await fetch(
-      `https://www.worldcoinindex.com/apiservice/ticker?key=2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649&label=ethbtc&fiat=btc`
+      `https://www.worldcoinindex.com/apiservice/ticker?key=2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649&label=ethbtc-ltcbtc&fiat=btc`
     );
     const data = await response.json();
     console.log(data.Markets);
-    data.Markets.foreach((Coin) => {
+    data.Markets.forEach((Coin) => {
       DOMSelectors.grid.insertAdjacentHTML(
         "beforeend",
-        `<section class="currency-name">
+        `<section class="currency">
       <div class="price-to-USD">
        <div class="price-to-Bitcoin"> 
      
        </div>
-         <h3 class="currenecy-name"> ${Coin.Name} </h3>
+         <h3 class="currency-name"> ${Coin.Name} </h3>
            <p class="price-to-USD">${Coin.Price}</p>
            <p class="Price-to-Bitcoin">${Coin.Volume_24h}</p>
        </div>
      </div> 
-   </section>1`
+   </section>`
       );
     });
-    console.log(Coin);
   } catch (error) {
     console.log(error);
     alert("something went wrong");
--- src/js/search.js
+++ src/js/search.js
@@ -10,24 +10,24 @@ const listen = function () {
         const response = await fetch(
           `https://www.worldcoinindex.com/apiservice/ticker?key=2oslZGi4GkzHKexsxM0XXFrn7FZ9uRkY649&label=${searchParams}&fiat=usd`
         );
-        console.log(data.Markets);
         const data = await response.json();
+        console.log(data.Markets);
         data.Markets.forEach((Coin) => {
           DOMSelectors.grid.insertAdjacentHTML(
             "beforeend",
-            `<section class="currency-name">
+            `<section class="currency">
           <div class="price-to-USD">
            <div class="price-to-Bitcoin">
          
            </div>
-             <h3 class="currenecy-name"> ${Coin.Name} </h3>
+             <h3 class="currency-name"> ${Coin.Name} </h3>
                <p class="price-to-USD">${Coin.Price}</p>
                <p class="Price-to-Bitcoin">${Coin.Volume_24h}</p>
            </div>
          </div> 
        </section>`
           );
-          console.log(Coin.Name);
+          console.log(DOMSelectors.grid);
         });
       } catch (error) {
         console.log(error);
