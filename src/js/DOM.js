cb378f38f72db82248a6ab5e50c8df70f7b98395
ryan-sliger <ryan.sliger22@sitechhs.com>
2021-05-28 09:41:26 -0400

    final final final changes

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,8 +1,6 @@
 const DOMSelectors = {
   grid: document.querySelector(".currency-name"),
   grid: document.querySelector(".currency"),
-  searchForm: document.getElementById("search-form"),
-  searchArea: document.getElementById("search-area"),
-};
+}
 
 export { DOMSelectors };
